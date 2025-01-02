64b9764912f130d6eeb7ab0af4b588798209e2d5
devin_mason <mvp@vastofi.com>
2025-01-02 13:29:05 -0500

    Change to configuration files for redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
   ],
