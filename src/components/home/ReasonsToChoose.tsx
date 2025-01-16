fb41f465bcc31495740f335d99f2ba64a857c287
devin_mason <mvp@vastofi.com>
2025-01-16 12:41:37 -0500

    FIX: Reasons to choose button update

--- src/components/home/ReasonsToChoose.tsx
+++ src/components/home/ReasonsToChoose.tsx
@@ -159,7 +159,9 @@ const ReasonsToChoose: React.FC = () => {
           {/* Card Footer */}
           <CardFooter className="flex justify-center pt-4">
               <div className='flex items-center bg-[#1A1A1A]/20 border border-[#1F1F1F] rounded-full px-5 py-2 space-x-2 hover:bg-[#1A1A1A]/40 transition'>
-                <Link href={card.link} className='text-white text-sm font-medium font-sora pr-4'>
+                <Link 
+                onPress={() => handleButtonClick(card.link)}
+                href={card.link} className='text-white text-sm font-medium font-sora pr-4'>
                   Learn More
                 </Link>
                 <Button 
