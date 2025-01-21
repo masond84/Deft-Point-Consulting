2150d3eefcf698f4f208e6fd64491de2c9db76fc
devin_mason <mvp@vastofi.com>
2025-01-21 15:24:18 -0500

    Update to site content

--- src/components/projects/Showcase.tsx
+++ src/components/projects/Showcase.tsx
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
-import { Card, CardHeader, CardBody, Button, Chip } from '@nextui-org/react';
+import { Card, CardHeader, CardBody, Button, Chip, Link } from '@nextui-org/react';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 import { motion } from "motion/react";
+import { useNavigate } from "react-router-dom";
 
 import { FaIndustry, FaLaptopCode, FaPalette } from 'react-icons/fa';
 import { TfiAngleDown, TfiAngleDoubleUp } from 'react-icons/tfi';
 import CardImage1 from '../../assets/images/DropdownCardImage1.png';
-import CardImage2 from '../../assets/images/DropdownCardImage2.jpg';
+import CardImage2 from '../../assets/images/DropdownCardImage2.png';
 
 // Card Icons
 import FigmaIcon from '../../assets/images/FigmaIcon.svg';
-
+import JSIcon from '../../assets/images/JSIcon.svg';
+import MUIIcon from '../../assets/images/MUIUIIcon.svg';
+import ReactIcon from '../../assets/images/ReactIcon.svg';
+import TailwindIcon from '../../assets/images/TailwindIcon.svg';
+import TSIcon from '../../assets/images/TSIcon.svg';
 
 const Showcase: React.FC = () => {
   const [expandedCard, setExpandedCard] = useState<number | null>(null);
+  const navigate = useNavigate();
 
   const toggleCard = (cardId: number): void => {
     setExpandedCard(expandedCard === cardId ? null : cardId); // Expand only the clicked card
@@ -96,36 +103,36 @@ const Showcase: React.FC = () => {
             </div>
             
             {/* Default Description */}
-            <div className="bg-[#0F0F0F] border border-[#1F1F1F] rounded-lg sm:ml-4 sm:mr-4 mb-4 p-4 sm:p-8 text-center sm:text-left">
+            <div className="bg-[#0F0F0F] border border-[#1F1F1F] rounded-lg sm:ml-4 sm:mr-4 -mb-4 sm:mb-4 p-4 sm:p-8 text-center sm:text-left">
               <h4 className="text-lg font-light text-white font-sora mb-4">Project Description</h4>
               <p className='text-xs text-gray-400 font-sora'>
-                A dynamic e-commerce platform that revolutionized online shopping for fashion enthusiasts. The website showcases the latest trends, offers personalized recommendations, and seamless checkout options.
+                A fully responsive website using React.js and Material UI. Designed for Minas Cleaning Service to display their service offerings and allow for online bookings.              
               </p>
             </div>
 
             {/* Dropdown Section */}
             {expandedCard === 1 && (
-              <div className="space-y-6 p-4 sm:p-8 bg-[#0f0f0f] border border-[#1F1F1F] rounded-lg sm:mx-4">
+              <div className="space-y-6 p-4 sm:p-8 bg-[#0f0f0f] border border-[#1F1F1F] rounded-lg">
                 {/* Project Details */}
                 <div className="flex flex-col sm:flex-row font-clash items-center sm:items-start justify-center sm:justify-between border border-[#1F1F1F] rounded-lg p-4 space-y-4 sm:space-y-0">
                   <div className='flex-1 text-center'>
                     <h4 className="font-light text-white font-sora">Category</h4>
-                    <p className="text-[#666666]">Technology</p>
+                    <p className="text-[#666666]">Website Development</p>
                   </div>
                   <div className="hidden sm:block w-px bg-[#1f1f1f] h-12 mx-4"></div>
                   <div className='flex-1 text-center'>
                     <h4 className="font-light text-white font-sora">Time Taken</h4>
-                    <p className="text-[#666666]">6 Months</p>
+                    <p className="text-[#666666]">3 Months</p>
                   </div>
                   <div className="hidden sm:block w-px bg-[#1f1f1f] h-12 mx-4"></div>
                   <div className='hidden sm:block flex-1 text-center'>
                     <h4 className="font-light text-white font-sora">Start Date</h4>
-                    <p className="text-[#666666]">Feb 1, 2023</p>
+                    <p className="text-[#666666]">Aug 20, 2024</p>
                   </div>
                   <div className="hidden sm:block w-px bg-[#1f1f1f] h-12 mx-4"></div>
                   <div className='hidden sm:block flex-1 text-center'>
                     <h4 className="font-light text-white font-sora">End Date</h4>
-                    <p className="text-[#666666]">Aug 1, 2023</p>
+                    <p className="text-[#666666]">Oct 16, 2024</p>
                   </div>
                 </div>
             
@@ -134,7 +141,12 @@ const Showcase: React.FC = () => {
                   <h4 className="text-md sm:text-lg font-light text-white font-sora text-center sm:text-left mb-2 sm:mb-0">Technologies Used</h4>
                   <div className="flex flex-wrap justify-center sm:justify-start ml-0 sm:ml-8 items-center gap-4 sm:gap-8">
                     {/* Example Technology Icons */}
-                    {[1, 2, 3, 4].map((_, index) => (
+                    {[
+                      { src: ReactIcon, alt: "React.js" },
+                      { src: TSIcon, alt: "TypeScript" },
+                      { src: TailwindIcon, alt: "Tailwind CSS" },
+                      { src: MUIIcon, alt: "Material UI" }
+                    ].map((tech, index) => (
                       <motion.div
                         key={index}
                         whileHover={{
@@ -146,8 +158,8 @@ const Showcase: React.FC = () => {
                         }}
                       >
                         <img
-                          src={FigmaIcon} // Replace with actual icon path
-                          alt={`Technology ${index + 1}`}
+                          src={tech.src}
+                          alt={tech.alt}
                           className="w-10 h-10 sm:w-12 sm:h-12"
                         />
                       </motion.div>
@@ -185,21 +197,28 @@ const Showcase: React.FC = () => {
                       size="lg"
                       variant="bordered"
                     >
-                      Development
+                      Full-Stack Development
                     </Chip>
                     <Chip
                       className="bg-transparent text-[#A6A6A6] border border-[#272727] rounded-full flex items-center px-3 py-1 sm:p-4 text-xs sm:text-sm font-clash"
                       size="lg"
                       variant="bordered"
                     >
-                      Development
+                      API Integration
                     </Chip>
                     <Chip
                       className="bg-transparent text-[#A6A6A6] border border-[#272727] rounded-full flex items-center px-3 py-1 sm:p-4 text-xs sm:text-sm font-clash"
                       size="lg"
                       variant="bordered"
                     >
-                      Development
+                      Mobile Optimization
+                    </Chip>
+                    <Chip
+                      className="bg-transparent text-[#A6A6A6] border border-[#272727] rounded-full flex items-center px-3 py-1 sm:p-4 text-xs sm:text-sm font-clash"
+                      size="lg"
+                      variant="bordered"
+                    >
+                      Email Marketing
                     </Chip>
                   </div>
                 </div>
@@ -212,7 +231,7 @@ const Showcase: React.FC = () => {
         <Card className="bg-glass-bg backdrop-blur-glass border  border-glass-border rounded-lg">
           {/* Card Header */}
           <CardHeader className="p-4">
-            <div className="bg-[#222222] rounded-lg overflow-hidden flex justify-center items-center h-fit">
+            <div className="bg-[#222222] shadow-glass rounded-lg overflow-hidden flex justify-center items-center h-fit">
               <img
                 src={CardImage2} // Replace with actual image
                 alt="Project Thumbnail"
@@ -225,8 +244,8 @@ const Showcase: React.FC = () => {
           <CardBody className="p-8 sm:p-6">
             {/* */}
             <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center sm:items-start text-center sm:text-left sm:pl-4 sm:pr-4 mb-4 space-y-4 sm:space-y-0">
-              <h3 className="text-lg sm:text-xl font-medium font-sora whitespace-nowrap">
-                TechGuru Platform
+              <h3 className="text-lg sm:text-xl font-medium font-sora flex-wrap">
+                Professional Car Detailing Site Template
               </h3>
               
               <div className="flex items-center gap-2">
@@ -264,42 +283,42 @@ const Showcase: React.FC = () => {
             <div className="bg-[#0F0F0F] border border-[#1F1F1F] rounded-lg sm:ml-4 sm:mr-4 mb-4 p-4 sm:p-8 text-center sm:text-left">
               <h4 className="text-lg font-light text-white font-sora mb-4">Project Description</h4>
                 <p className="text-xs text-gray-400 font-sora">
-                  A cutting-edge platform for technology enthusiasts, designed to streamline workflows and enhance collaboration among teams.
+                  Developed a full-site template for a mobile detailing business focusing on creating a visually appealing and functional design using Figma.
                 </p>
             </div>
 
             {/* Dropdown Section */}
             {expandedCard === 2 && (
-              <div className="space-y-6 p-4 sm:p-8 bg-[#0f0f0f] border border-[#1F1F1F] rounded-lg sm:mx-4">
+              <div className="space-y-6 p-4 sm:p-8 bg-[#0f0f0f] border border-[#1F1F1F] rounded-lg mx-0 sm:mx-4">
                 {/* Project Details */}
                 <div className="flex flex-col sm:flex-row font-clash items-center sm:items-start justify-center sm:justify-between border border-[#1F1F1F] rounded-lg p-4 space-y-4 sm:space-y-0">
                   <div className='flex-1 text-center'>
                     <h4 className="font-light text-white font-sora">Category</h4>
-                    <p className="text-[#666666]">Technology</p>
+                    <p className="text-[#666666]">Design & Branding</p>
                   </div>
                   <div className="hidden sm:block w-px bg-[#1f1f1f] h-12 mx-4"></div>
                   <div className='flex-1 text-center'>
                     <h4 className="font-light text-white font-sora">Time Taken</h4>
-                    <p className="text-[#666666]">6 Months</p>
+                    <p className="text-[#666666]">2 Weeks</p>
                   </div>
                   <div className="hidden sm:block w-px bg-[#1f1f1f] h-12 mx-4"></div>
                   <div className='hidden sm:block flex-1 text-center'>
                     <h4 className="font-light text-white font-sora">Start Date</h4>
-                    <p className="text-[#666666]">Feb 1, 2023</p>
+                    <p className="text-[#666666]">Dec 16, 2024</p>
                   </div>
                   <div className="hidden sm:block w-px bg-[#1f1f1f] h-12 mx-4"></div>
                   <div className='hidden sm:block flex-1 text-center'>
                     <h4 className="font-light text-white font-sora">End Date</h4>
-                    <p className="text-[#666666]">Aug 1, 2023</p>
+                    <p className="text-[#666666]">Jan 3, 2025</p>
                   </div>
                 </div>
             
                 {/* Technologies Used */}
                 <div className="flex flex-col sm:flex-row font-clash items-center sm:items-start justify-center sm:justify-start border border-[#1F1F1F] rounded-lg p-4 sm:pl-12">
-                  <h4 className="text-md sm:text-lg font-light text-white font-sora text-center sm:text-left mb-2 sm:mb-0">Technologies Used</h4>
+                  <h4 className="text-md sm:text-lg font-light text-white font-sora text-center sm:text-left mb-2 sm:mb-0 pt-2">Technologies Used</h4>
                   <div className="flex flex-wrap justify-center sm:justify-start ml-0 sm:ml-8 items-center gap-4 sm:gap-8">
                     {/* Example Technology Icons */}
-                    {[1, 2, 3, 4].map((_, index) => (
+                    {[1].map((_, index) => (
                       <motion.div
                         key={index}
                         whileHover={{
@@ -350,21 +369,21 @@ const Showcase: React.FC = () => {
                       size="lg"
                       variant="bordered"
                     >
-                      Development
+                      UI/UX Design
                     </Chip>
                     <Chip
                       className="bg-transparent text-[#A6A6A6] border border-[#272727] rounded-full flex items-center px-3 py-1 sm:p-4 text-xs sm:text-sm font-clash"
                       size="lg"
                       variant="bordered"
                     >
-                      Development
+                      Branding Strategy Development
                     </Chip>
                     <Chip
                       className="bg-transparent text-[#A6A6A6] border border-[#272727] rounded-full flex items-center px-3 py-1 sm:p-4 text-xs sm:text-sm font-clash"
                       size="lg"
                       variant="bordered"
                     >
-                      Development
+                      Prototyping and Wirefreaming
                     </Chip>
                   </div>
                 </div>
@@ -372,6 +391,21 @@ const Showcase: React.FC = () => {
             )}
           </CardBody>
         </Card>
+      </div>
+
+      {/* Bottom Button */}
+      <div className="text-center mt-6">
+        <Link
+          href="/portfolio"
+          className="no-underline" // Prevent underline styling
+        >
+            <Button 
+            onPress={() => navigate('/portfolio#showcase')}
+            className="px-6 py-3 rounded-full border border-[#262626] bg-[#1A1A1A]/20 text-white font-sora hover:bg-[#1A1A1A]/40 text-sm font-light"
+            >
+              View More <ArrowRightAltIcon className="ml-2" />
+            </Button>
+        </Link>
       </div>
     </section>
   );
