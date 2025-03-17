cb3a40a655f28154a25c549b573ec5df120018f9
devin_mason <mvp@vastofi.com>
2025-03-16 20:26:14 -0400

    Initialization of Google Analytics With Production Url

--- src/ga.ts
+++ src/ga.ts
@@ -1,7 +1,9 @@
 import ReactGA from 'react-ga4';
 
 const TRACKING_ID = "G-8EHPQDX2MX";
+const PRODUCTION_ID = "G-BTW851KXSF";
 
 export const initializeGA = () => {
     ReactGA.initialize(TRACKING_ID);
+    ReactGA.initialize(PRODUCTION_ID, { gaOptions: { name: 'production' } });
 };
